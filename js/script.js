3e447bcd31a8645464f20fe36205453ce12b3888
aleksslav <aleksslav@gmail.com>
2020-06-26 19:33:15 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,43 +1,42 @@
 // первый слайдер //
-
 var promoSliderList = document.querySelector(".slider-list");
 
-var removePromoSliderActive = function () {
-    document.querySelector(".slider-control-button-current").classList.remove("slider-control-button-current");
-    document.querySelector(".slide-current").classList.remove("slide-current");
-  }
+var removePromoSliderActive = function() {
+	document.querySelector(".slider-control-button-current").classList.remove("slider-control-button-current");
+	document.querySelector(".slide-current").classList.remove("slide-current");
+}
 
-  document.querySelectorAll(".slider-control-button").forEach(function (button, i) {
-    button.addEventListener("click", function (evt) {
-      evt.preventDefault();
+document.querySelectorAll(".slider-control-button").forEach(function(button, i) {
+	button.addEventListener("click", function(evt) {
+		evt.preventDefault();
 
-      removePromoSliderActive();
-      button.classList.add("slider-control-button-current");
-      promoSliderList.children[i].classList.add("slide-current");
-    })
-  });
+		removePromoSliderActive();
+		button.classList.add("slider-control-button-current");
+		promoSliderList.children[i].classList.add("slide-current");
+	})
+});
 
-  // второй слайдер //
+// второй слайдер //
 
-  var servicesSliderList = document.querySelector(".services-description");
-  var servicesSliderControl = document.querySelector(".services-buttons");
+var servicesSliderList = document.querySelector(".services-description");
+var servicesSliderControl = document.querySelector(".services-buttons");
 
-  var removeServicesSliderActive = function () {
-    servicesSliderControl.querySelector(".bottom-slider-button-active").classList.remove("bottom-slider-button-active");
-    document.querySelector(".slide-bottom-current").classList.remove("slide-bottom-current");
-  }
+var removeServicesSliderActive = function() {
+	servicesSliderControl.querySelector(".bottom-slider-button-active").classList.remove("bottom-slider-button-active");
+	document.querySelector(".slide-bottom-current").classList.remove("slide-bottom-current");
+}
 
-  servicesSliderControl.querySelectorAll(".bottom-slider-button").forEach(function (button, i) {
-    button.addEventListener("click", function (evt) {
-      evt.preventDefault();
-  
-      removeServicesSliderActive();
-      button.classList.add("bottom-slider-button-active");
-      servicesSliderList.children[i].classList.add("slide-bottom-current");
-    })
-  });
+servicesSliderControl.querySelectorAll(".bottom-slider-button").forEach(function(button, i) {
+	button.addEventListener("click", function(evt) {
+		evt.preventDefault();
 
-  // Форма письма //
+		removeServicesSliderActive();
+		button.classList.add("bottom-slider-button-active");
+		servicesSliderList.children[i].classList.add("slide-bottom-current");
+	})
+});
+
+// Форма письма //
 
 var letterLink = document.querySelector(".letter-link");
 var letterModal = document.querySelector(".modal-write-us");
@@ -51,71 +50,71 @@ var storage = "";
 var ESC_KEYCODE = 27;
 
 try {
-  storage = localStorage.getItem("login");
+	storage = localStorage.getItem("login");
 } catch (err) {
-  isStorageSupport = false;
+	isStorageSupport = false;
 }
 
-  letterLink.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    letterModal.classList.add("modal-show");
-    if (storage) {
-      userName.value = storage;
-      userEmail.focus();
-    } else {
-      userName.focus();
-    }
+letterLink.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	letterModal.classList.add("modal-show");
+	if (storage) {
+		userName.value = storage;
+		userEmail.focus();
+	} else {
+		userName.focus();
+	}
 });
 
-  letterClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    letterModal.classList.remove("modal-show");
-    letterModal.classList.remove("modal-error");
+letterClose.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	letterModal.classList.remove("modal-show");
+	letterModal.classList.remove("modal-error");
 });
 
-  letterForm.addEventListener("submit", function (evt) {
-    if (!userName.value || !userEmail.value) {
-      evt.preventDefault();
-      letterModal.classList.remove("modal-error");
-      letterModal.offsetWidth = letterModal.offsetWidth;
-      letterModal.classList.add("modal-error");
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem("login", userName.value);
-      }
-    }
-  });
+letterForm.addEventListener("submit", function(evt) {
+	if (!userName.value || !userEmail.value) {
+		evt.preventDefault();
+		letterModal.classList.remove("modal-error");
+		letterModal.offsetWidth = letterModal.offsetWidth;
+		letterModal.classList.add("modal-error");
+	} else {
+		if (isStorageSupport) {
+			localStorage.setItem("login", userName.value);
+		}
+	}
+});
 
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (letterModal.classList.contains("modal-show")) {
-        evt.preventDefault();
-        letterModal.classList.remove("modal-show");
-        letterModal.classList.remove("modal-error");
-      }
-    }
-  });
+window.addEventListener("keydown", function(evt) {
+	if (evt.keyCode === 27) {
+		if (letterModal.classList.contains("modal-show")) {
+			evt.preventDefault();
+			letterModal.classList.remove("modal-show");
+			letterModal.classList.remove("modal-error");
+		}
+	}
+});
 
-  // Форма Карты //
-  var mapOpen = document.querySelector(".contacts-map-img");
-  var mapModal = document.querySelector(".modal.modal-map");
-  var mapClose = mapModal.querySelector(".modal-close");
+// Форма Карты //
+var mapOpen = document.querySelector(".contacts-map-img");
+var mapModal = document.querySelector(".modal.modal-map");
+var mapClose = mapModal.querySelector(".modal-close");
 
-  mapOpen.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapModal.classList.add("modal-show");
-  });
-  
-  mapClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapModal.classList.remove("modal-show");
-  });
+mapOpen.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	mapModal.classList.add("modal-show");
+});
 
-   window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (mapModal.classList.contains("modal-show")) {
-        evt.preventDefault();
-        mapModal.classList.remove("modal-show");
-      }
-    }
-  });
+mapClose.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	mapModal.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+	if (evt.keyCode === 27) {
+		if (mapModal.classList.contains("modal-show")) {
+			evt.preventDefault();
+			mapModal.classList.remove("modal-show");
+		}
+	}
+});
